568116ebcc2dad00c07818b3bb6f9ac9a99b2ae8
mrlazibabi <anhltse170584@fpt.edu.vn>
2024-10-14 00:37:15 +0700

    #Health collectible

--- Assets/Script/BlockItem/PowerUp.cs
+++ Assets/Script/BlockItem/PowerUp.cs
@@ -2,36 +2,45 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
-    
-    public enum Type
-    {
-        Coin,
-        ExtraLife,
-    }
 
-    public Type type;
+    //public enum Type
+    //{
+    //    Coin,
+    //    ExtraLife,
+    //}
 
+    //public Type type;
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Player"))
+    //    {
+    //        Collect(collision.gameObject);
+    //    }
+    //}
+
+    //private void Collect(GameObject player)
+    //{
+    //    switch (type)
+    //    {
+    //        case Type.Coin:
+    //            BlockCoin.Instance.AddCoin();
+    //            break;
+
+    //        case Type.ExtraLife:
+    //            BlockCoin.Instance.AddLife();
+    //            break;
+    //    }
+
+    //    Destroy(gameObject);
+    //}
+    [SerializeField] private float healthValue;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.tag == "Player")
         {
-            Collect(collision.gameObject);
+            collision.GetComponent<Health>().AddHealth(healthValue);
+            gameObject.SetActive(false);
         }
-    }
-
-    private void Collect(GameObject player)
-    {
-        switch (type)
-        {
-            case Type.Coin:
-                BlockCoin.Instance.AddCoin();
-                break;
-
-            case Type.ExtraLife:
-                BlockCoin.Instance.AddLife();
-                break;
-        }
-
-        Destroy(gameObject);
     }
 }
---
+++ Assets/Script/Enermy/GoombaAttack.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoombaAttack : MonoBehaviour
+{
+    [SerializeField] private float damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            collision.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+
+
+}
