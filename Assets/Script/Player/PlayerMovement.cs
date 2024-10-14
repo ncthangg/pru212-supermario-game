b50e1ee8c38a08cabe6e76464edb89043f25bb16
ttlihao <thienphan20122003@gmail.com>
2024-10-14 16:47:48 +0700

    adjust UI

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -3,23 +3,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private AudioClip walkSound;
+    [SerializeField] private AudioClip jumpSound;
     private Rigidbody2D body;
     private Animator anim;
     private bool grounded;
+    private AudioSource audioSource;
 
     private void Awake()
     {
-        //Grab references for rigidbody and animator from object
+        // Grab references for rigidbody, animator, and audio source from the object
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
-        //Flip charater when moving left-right
+        // Flip character when moving left-right
         if (horizontalInput > 0.01f)
         {
             transform.localScale = new Vector3(3, 3, 3);
@@ -29,13 +33,13 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector3(-3, 3, 3);
         }
 
-        //Jumping 
+        // Jumping
         if (Input.GetKey(KeyCode.W) && grounded)
         {
             Jump();
         }
 
-        //Set animator parameter
+        // Set animator parameter
         anim.SetBool("run", horizontalInput != 0);
         anim.SetBool("grounded", grounded);
     }
@@ -44,13 +48,21 @@ public class PlayerMovement : MonoBehaviour
     {
         body.velocity = new Vector2(body.velocity.x, 15f);
         anim.SetTrigger("jump");
+        audioSource.PlayOneShot(jumpSound); // Play jump sound effect
         grounded = false;
-
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
             grounded = true;
+    }
+
+    public void PlayFootstepSound()
+    {
+        if (grounded)
+        {
+            audioSource.PlayOneShot(walkSound); // Play walking sound effect
+        }
     }
 }
