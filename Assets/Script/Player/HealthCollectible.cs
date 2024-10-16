d26709d382899d123f8a963dcf547fa661be1df4
nhatquang1605 <honhatquang1605@gmail.com>
2024-10-16 20:29:51 +0700

    FixHealthCollec

--- Assets/Script/Player/HealthCollectible.cs
+++ Assets/Script/Player/HealthCollectible.cs
@@ -1,16 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
 {
-    [SerializeField] private float healthValue;
+    [SerializeField] private float healthValue;  // Giá trị máu được cộng khi nhặt
+    private Collider2D col;                     // Collider của cục máu
+
+    private void Start()
+    {
+        // Lấy Collider2D của cục máu
+        col = GetComponent<Collider2D>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+        // Kiểm tra nếu đối tượng va chạm là Player
+        if (collision.CompareTag("Player"))
         {
+            // Thêm máu cho người chơi
             collision.GetComponent<Health>().AddHealth(healthValue);
-            gameObject.SetActive(false);
+
+            // Xóa Collider để cục máu không còn tương tác
+            if (col != null)
+            {
+                Destroy(col);  // Xóa collider
+            }
+
+            // Ẩn cục máu hoặc có thể dùng gameObject.SetActive(false);
+            Destroy(gameObject);
         }
     }
 }
